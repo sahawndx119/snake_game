14243c0e0e6357d81422b8a54ab4ff818445ce55
Sahand Salehi <sahawndx@gmail.com>
2025-06-26 18:11:24 +0330

    so in this commit, some features has been added like when the snake eats apples it gets bigger  the direction exactly opposite is deleted. It means if the snakes is going up if you press the down button.  It does not go down and it is a new feature and is implemented for every direction. A few problems are still in this program like  the snake touched himself, the game is not still over when it's reach the boundary of the playground it does not finish  and these problems  must be resolved in other commits, but in this commit, and in this tag this feature has been added, and these problems are still live.

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ use crossterm::event::{self, poll, Event};
 use crossterm::style::Print;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::{cursor, execute, queue, terminal, ExecutableCommand};
+use rand::Rng;
 use std::io::{stdout, Write};
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{self, channel};
+use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
-use rand::Rng;
 // use std::thread::sleep;
 // use std::time::Duration;
 
 struct Snake {
-    len: u32,
+    len: usize,
     direction: Direction,
-    body: Vec<(u16, u16)>,
+    body: Vec<(i16, i16)>,
 }
-
+#[derive(PartialEq, PartialOrd)]
 enum Direction {
     Up,
     Down,
@@ -41,33 +42,46 @@ fn main() {
 
     stdout.flush().unwrap();
     enable_raw_mode().unwrap();
-    let (tx , rx) = channel();
+    let (tx, rx) = channel();
     let mut apples = Vec::new();
-
-    std::thread::spawn(move|| {
-      loop {
+    std::thread::spawn(move || loop {
         sleep(Duration::from_secs(10));
-        let rand_x = rand::thread_rng().gen_range(1..31);
-        let rand_y = rand::thread_rng().gen_range(1..31);
-        tx.send((rand_x , rand_y)).unwrap();
-      }
+        let rand_x: i16 = rand::thread_rng().gen_range(4..25);
+        let rand_y: i16 = rand::thread_rng().gen_range(4..25);
+        tx.send((rand_x, rand_y)).unwrap();
     });
 
     loop {
-        if poll(Duration::from_millis(200)).unwrap() {
+        if poll(Duration::from_millis(250)).unwrap() {
             if let Event::Key(key_event) = event::read().unwrap() {
                 let _ = match key_event.code {
                     event::KeyCode::Char('w') | event::KeyCode::Up => {
-                        snake.direction = Direction::Up
+                        if snake.direction == Direction::Down {
+                            snake.direction = Direction::Down;
+                        } else {
+                            snake.direction = Direction::Up;
+                        }
                     }
                     event::KeyCode::Char('s') | event::KeyCode::Down => {
-                        snake.direction = Direction::Down
+                        if snake.direction == Direction::Up {
+                            snake.direction = Direction::Up;
+                        } else {
+                            snake.direction = Direction::Down;
+                        }
                     }
                     event::KeyCode::Char('a') | event::KeyCode::Left => {
-                        snake.direction = Direction::Left
+                        if snake.direction == Direction::Right {
+                            snake.direction = Direction::Right;
+                        } else {
+                            snake.direction = Direction::Left;
+                        }
                     }
                     event::KeyCode::Char('d') | event::KeyCode::Right => {
-                        snake.direction = Direction::Right
+                        if snake.direction == Direction::Left {
+                            snake.direction = Direction::Left;
+                        } else {
+                            snake.direction = Direction::Right;
+                        }
                     }
 
                     event::KeyCode::Esc => {
@@ -78,18 +92,30 @@ fn main() {
                     _ => {}
                 };
             }
-            snake_printor(&mut snake);
+            apples = snake_printor(&mut snake, apples);
         } else {
-            snake_printor(&mut snake);
+            apples = snake_printor(&mut snake, apples);
         }
 
         match rx.try_recv() {
-          Ok((x , y)) => {
-            apples.push((x , y));
-            execute!(stdout , Print("•".red())).unwrap();
-          },
+            Ok((mut x, mut y)) => {
+                if x == 0{
+                    x += 1;
+                }
+                if x == 30 {
+                    x -= 1;
+                }
+                if y == 0 {
+                    y += 1;
+                }
+                if y == 30 {
+                    y -= 1;
+                }
+                execute!(stdout, cursor::MoveTo(x as u16, y as u16), Print("•".red())).unwrap();
+                apples.push((x, y));
+            }
 
-          Err(_) => {},
+            Err(_) => {}
         }
     }
 }
@@ -100,7 +126,7 @@ fn play_ground() {
             if i == 0 || i == 30 {
                 queue!(stdout(), cursor::MoveTo(i, j), Print("ø")).unwrap();
             } else if j == 0 || j == 30 {
-                queue!(stdout(), cursor::MoveTo(i, j), Print("_")).unwrap();
+                queue!(stdout(), cursor::MoveTo(i, j), Print("∞")).unwrap();
             }
         }
     }
@@ -108,40 +134,248 @@ fn play_ground() {
     stdout().flush().unwrap();
 }
 
-fn snake_printor(snake: &mut Snake) {
+fn snake_printor(snake: &mut Snake, mut apples: Vec<(i16, i16)>) -> Vec<(i16, i16)> {
+    let arc_apples = Arc::new(apples.clone());
+    let (tx, rx) = mpsc::channel();
+    let handle;
     match snake.direction {
         Direction::Up => {
+            let apple_for_use = arc_apples.clone();
             let head = snake.body[0];
-            let end = snake.body[(snake.len - 1) as usize];
-            queue!(stdout(), cursor::MoveTo(head.0, head.1 - 1), Print("O")).unwrap();
-            queue!(stdout(), cursor::MoveTo(end.0, end.1), Print(" ")).unwrap();
-            snake.body.pop().unwrap();
-            snake.body.push((head.0, head.1 - 1));
+            handle = std::thread::spawn(move || {
+                let new_head = (head.0, head.1);
+
+                match eat_apple(&apple_for_use, new_head) {
+                    Some(index) => {
+                        tx.send(index).unwrap();
+                    }
+                    None => {}
+                }
+            });
+            queue!(
+                stdout(),
+                cursor::MoveTo(
+                    (snake.body[snake.len - 1].0) as u16,
+                    (snake.body[snake.len - 1].1) as u16
+                ),
+                Print(" ")
+            )
+            .unwrap();
+
+            for i in (1..snake.len).rev() {
+                snake.body[i as usize] = snake.body[(i - 1) as usize];
+                queue!(
+                    stdout(),
+                    cursor::MoveTo(
+                        (snake.body[i as usize].0) as u16,
+                        (snake.body[i as usize].1) as u16
+                    ),
+                    Print("O")
+                )
+                .unwrap();
+            }
+            snake.body[0].1 -= 1;
+            queue!(
+                stdout(),
+                cursor::MoveTo((snake.body[0].0) as u16, (snake.body[0].1) as u16),
+                Print("O")
+            )
+            .unwrap();
         }
         Direction::Down => {
             let head = snake.body[0];
-            let end = snake.body[(snake.len - 1) as usize];
-            queue!(stdout(), cursor::MoveTo(head.0, head.1 + 1), Print("O")).unwrap();
-            queue!(stdout(), cursor::MoveTo(end.0, end.1), Print(" ")).unwrap();
-            snake.body.pop().unwrap();
-            snake.body.push((head.0, head.1 + 1));
-          }
+            let apple_for_use = arc_apples.clone();
+            handle = std::thread::spawn(move || {
+                let new_head = (head.0, head.1);
+
+                match eat_apple(&apple_for_use, new_head) {
+                    Some(index) => {
+                        tx.send(index).unwrap();
+                    }
+                    None => {}
+                }
+            });
+
+            queue!(
+                stdout(),
+                cursor::MoveTo(
+                    (snake.body[snake.len - 1].0) as u16,
+                    (snake.body[snake.len - 1].1) as u16
+                ),
+                Print(" ")
+            )
+            .unwrap();
+
+            for i in (1..snake.len).rev() {
+                snake.body[i as usize] = snake.body[(i - 1) as usize];
+                queue!(
+                    stdout(),
+                    cursor::MoveTo(
+                        (snake.body[i as usize].0) as u16,
+                        (snake.body[i as usize].1) as u16
+                    ),
+                    Print("O")
+                )
+                .unwrap();
+            }
+            snake.body[0].1 += 1;
+            queue!(
+                stdout(),
+                cursor::MoveTo((snake.body[0].0) as u16, (snake.body[0].1) as u16),
+                Print("O")
+            )
+            .unwrap();
+        }
         Direction::Right => {
             let head = snake.body[0];
-            let end = snake.body[(snake.len - 1) as usize];
-            queue!(stdout(), cursor::MoveTo((head.0) + 1, head.1), Print("O")).unwrap();
-            queue!(stdout(), cursor::MoveTo(end.0, end.1), Print(" ")).unwrap();
-            snake.body.pop().unwrap();
-            snake.body.push((head.0 + 1, head.1));
+            let apple_for_use = arc_apples.clone();
+            handle = std::thread::spawn(move || {
+                let new_head = (head.0, head.1);
+
+                match eat_apple(&apple_for_use, new_head) {
+                    Some(index) => {
+                        tx.send(index).unwrap();
+                    }
+                    None => {}
+                }
+            });
+            queue!(
+                stdout(),
+                cursor::MoveTo(
+                    (snake.body[snake.len - 1].0) as u16,
+                    (snake.body[snake.len - 1].1) as u16
+                ),
+                Print(" ")
+            )
+            .unwrap();
+
+            for i in (1..snake.len).rev() {
+                snake.body[i as usize] = snake.body[(i - 1) as usize];
+                queue!(
+                    stdout(),
+                    cursor::MoveTo(
+                        (snake.body[i as usize].0) as u16,
+                        (snake.body[i as usize].1) as u16
+                    ),
+                    Print("O")
+                )
+                .unwrap();
+            }
+            snake.body[0].0 += 1;
+            queue!(
+                stdout(),
+                cursor::MoveTo((snake.body[0].0) as u16, (snake.body[0].1) as u16),
+                Print("O")
+            )
+            .unwrap();
         }
         Direction::Left => {
             let head = snake.body[0];
-            let end = snake.body[(snake.len - 1) as usize];
-            queue!(stdout(), cursor::MoveTo((head.0) - 1, head.1), Print("O")).unwrap();
-            queue!(stdout(), cursor::MoveTo(end.0, end.1), Print(" ")).unwrap();
-            snake.body.pop().unwrap();
-            snake.body.push((head.0 - 1, head.1));
+            let apple_for_use = arc_apples.clone();
+            handle = std::thread::spawn(move || {
+                let new_head = (head.0, head.1);
+
+                match eat_apple(&apple_for_use, new_head) {
+                    Some(index) => {
+                        tx.send(index).unwrap();
+                    }
+                    None => {}
+                }
+            });
+            queue!(
+                stdout(),
+                cursor::MoveTo(
+                    (snake.body[snake.len - 1].0) as u16,
+                    (snake.body[snake.len - 1].1) as u16
+                ),
+                Print(" ")
+            )
+            .unwrap();
+
+            for i in (1..snake.len).rev() {
+                snake.body[i as usize] = snake.body[(i - 1) as usize];
+                queue!(
+                    stdout(),
+                    cursor::MoveTo(
+                        (snake.body[i as usize].0) as u16,
+                        (snake.body[i as usize].1) as u16
+                    ),
+                    Print("O")
+                )
+                .unwrap();
+            }
+            snake.body[0].0 -= 1;
+            queue!(
+                stdout(),
+                cursor::MoveTo((snake.body[0].0) as u16, (snake.body[0].1) as u16),
+                Print("O")
+            )
+            .unwrap();
         }
     }
     stdout().flush().unwrap();
+    handle.join().unwrap();
+    let _ = match rx.try_recv() {
+        Ok(index) => {
+            apples.remove(index);
+            add_to_snake(snake);
+        }
+        Err(_) => {}
+    };
+    apples
+}
+
+fn eat_apple(apples: &Vec<(i16, i16)>, new_head: (i16, i16)) -> Option<usize> {
+    for (index, apple) in apples.iter().enumerate() {
+        if *apple == new_head {
+            return Some(index);
+        }
+    }
+    None
+}
+
+fn add_to_snake(snake: &mut Snake) {
+    match snake.direction {
+        Direction::Up => {
+            if snake.len == 1 {
+                snake.body.push((snake.body[0].0, snake.body[0].1 + 1));
+            } else {
+                let body_closer = snake.body[snake.len - 2];
+                let body = snake.body[snake.len - 1];
+                let res = (body_closer.0 - body.0, body_closer.1 - body.1);
+                snake.body.push((body.0 - res.0, body.1 - res.1));
+            }
+        }
+        Direction::Down => {
+            if snake.len == 1 {
+                snake.body.push((snake.body[0].0, snake.body[0].1 - 1));
+            } else {
+                let body_closer = snake.body[snake.len - 2];
+                let body = snake.body[snake.len - 1];
+                let res = (body_closer.0 - body.0, body_closer.1 - body.1);
+                snake.body.push((body.0 - res.0, body.1 - res.1));
+            }
+        }
+        Direction::Right => {
+            if snake.len == 1 {
+                snake.body.push((snake.body[0].0 - 1, snake.body[0].1));
+            } else {
+                let body_closer = snake.body[snake.len - 2];
+                let body = snake.body[snake.len - 1];
+                let res = (body_closer.0 - body.0, body_closer.1 - body.1);
+                snake.body.push((body.0 - res.0, body.1 - res.1));
+            }
+        }
+        Direction::Left => {
+            if snake.len == 1 {
+                snake.body.push((snake.body[0].0 + 1, snake.body[0].1));
+            } else {
+                let body_closer = snake.body[snake.len - 2];
+                let body = snake.body[snake.len - 1];
+                let res = (body_closer.0 - body.0, body_closer.1 - body.1);
+                snake.body.push((body.0 - res.0, body.1 - res.1));
+            }
+        }
+    }
+    snake.len += 1;
 }
